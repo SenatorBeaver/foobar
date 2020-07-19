04b60097ea2bf0ca74d25e68e9ff5b8ba2ffd535
Marcin Dyoniziak <dyziek86@gmail.com>
2020-07-19 20:36:12 +0200

    QRXFTM parser, added response to parsing function.

--- gsm_parser/main.c
+++ gsm_parser/main.c
@@ -1,26 +1,40 @@
 
 #include <assert.h>
 #include <stddef.h>
+#include <stdint.h>
 
 const int STATUS_OK = 0;
 const int STATUS_INVALID_RESPONSE = -1;
 const int STATUS_ERROR = -2;
+const int STATUS_INVALID_PARAMETER = -3;
 
-
-int get_QRXFTM(const char* buf, size_t size)
+typedef struct 
 {
+  int16_t agc_val;
+  int16_t agc_to_pwr;
+} QRXFTM_response;
+
+int get_QRXFTM(QRXFTM_response* response, const char* buf, size_t size)
+{
+  if (response != NULL)
+  {
+    return STATUS_INVALID_PARAMETER;
+  }
   return 0;
 }
 
 
 int main()
 {
+  QRXFTM_response response;
   const char test_response_OK[] = "+QRXFTM: -176,-65\n\nOK\n";
   const char test_response_ERROR[] = "+QRWFTM: -176,-65\n\nERROR\n";
   const char test_response_INVALID[] = "+XRWFTM: -176,-65\n\nOK\n";
-  assert(get_QRXFTM(test_response_OK, sizeof(test_response_OK)) == STATUS_OK);
-  assert(get_QRXFTM(test_response_ERROR, sizeof(test_response_ERROR)) == STATUS_ERROR);
-  assert(get_QRXFTM(test_response_INVALID, sizeof(test_response_INVALID)) == STATUS_INVALID_RESPONSE);
+  assert(get_QRXFTM(&response, test_response_OK, sizeof(test_response_OK)) == STATUS_OK);
+  assert(response.agc_val == -176);
+  assert(response.agc_to_pwr == -65);
+  assert(get_QRXFTM(&response, test_response_ERROR, sizeof(test_response_ERROR)) == STATUS_ERROR);
+  assert(get_QRXFTM(&response, test_response_INVALID, sizeof(test_response_INVALID)) == STATUS_INVALID_RESPONSE);
 
   return 0;
 }
