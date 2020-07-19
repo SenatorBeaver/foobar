72acdfa7c72b2868880d14c2430db92967e04800
Marcin Dyoniziak <dyziek86@gmail.com>
2020-07-19 20:29:56 +0200

    QRXFTM parser, added basic tests.

--- gsm_parser/main.c
+++ gsm_parser/main.c
@@ -1,5 +1,26 @@
 
+#include <assert.h>
+#include <stddef.h>
+
+const int STATUS_OK = 0;
+const int STATUS_INVALID_RESPONSE = -1;
+const int STATUS_ERROR = -2;
+
+
+int get_QRXFTM(const char* buf, size_t size)
+{
+  return 0;
+}
+
+
 int main()
 {
+  const char test_response_OK[] = "+QRXFTM: -176,-65\n\nOK\n";
+  const char test_response_ERROR[] = "+QRWFTM: -176,-65\n\nERROR\n";
+  const char test_response_INVALID[] = "+XRWFTM: -176,-65\n\nOK\n";
+  assert(get_QRXFTM(test_response_OK, sizeof(test_response_OK)) == STATUS_OK);
+  assert(get_QRXFTM(test_response_ERROR, sizeof(test_response_ERROR)) == STATUS_ERROR);
+  assert(get_QRXFTM(test_response_INVALID, sizeof(test_response_INVALID)) == STATUS_INVALID_RESPONSE);
+
   return 0;
 }
